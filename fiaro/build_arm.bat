3010027301e2307a78849ea8230ecb498e3307bb
reloZid <relozid@logixisland.ch>
2015-12-31 12:55:27 +0100

    optimized ARM build batch file

--- fiaro/build_arm.bat
+++ fiaro/build_arm.bat
@@ -1,12 +1,14 @@
 @echo off
+prompt $G 
 cd %~dp0
+rd /s /q target\thumbv7m-none-eabi 2>nul
 mkdir target\thumbv7m-none-eabi 2>nul
 @echo on
 
-rustc -C opt-level=0 -Z no-landing-pads -g -L libs_arm ^
+rustc -C opt-level=0 -g -Z no-landing-pads -L libs_arm ^
 --target thumbv7m-none-eabi ^
 --crate-type lib ^
 --crate-name fiaro ^
+--emit obj ^
 --out-dir target/thumbv7m-none-eabi ^
---emit obj,link ^
 src/lib_target.rs
